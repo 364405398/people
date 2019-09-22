a1bd83549665ae3b15e651c5ce43f15e87f1517f
364405398 <55659243+364405398@users.noreply.github.com>
2019-09-22 22:16:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
-<?php  session_start(); 
-unset($_SESSION['id']) ?>
+
 <html>
 <head>	
 </head>
 
 <body >
-<?php 
-include_once("database.php");
-getConnect();
-$sql = "SELECT * FROM player";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["pid"]. " - Name: " . $row["pname"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-?>
+
 hello!
 </body>
 </html>
